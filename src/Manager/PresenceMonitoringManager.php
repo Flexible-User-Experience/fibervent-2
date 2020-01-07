c34f89af6127f8118a9ed9ee7451925395349f5a
David Romaní <david@flux.cat>
2020-01-07 12:16:58 +0100

    improve PresenceMonitoringManager

--- src/Manager/PresenceMonitoringManager.php
+++ src/Manager/PresenceMonitoringManager.php
@@ -4,6 +4,7 @@ namespace App\Manager;
 
 use App\Entity\PresenceMonitoring;
 use App\Entity\User;
+use App\Enum\PresenceMonitoringCategoryEnum;
 use App\Repository\PresenceMonitoringRepository;
 
 /**
@@ -54,16 +55,39 @@ class PresenceMonitoringManager
      */
     public function createFullMonthItemsListByOperatorYearAndMonth(User $operator, $year, $month)
     {
+        $result = array();
         $items = $this->getItemsByOperatorYearAndMonthSortedByDate($operator, $year, $month);
+        $currentItem = array_shift($items);
+        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month , $year);
+        for ($dayNumber = 1; $dayNumber <= $daysInMonth; $dayNumber++) {
+            $day = new \DateTime();
+            $day->setDate($year, $month, $dayNumber);
+            if ($currentItem && $currentItem->getDateString() == $day->format('d/m/Y')) {
+                array_push($result, $currentItem);
+                $currentItem = array_shift($items);
+            } else {
+                array_push($result, $this->buildEmptyItemForDay($operator, $day));
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param User $operator
+     * @param \DateTime $day
+     *
+     * @return PresenceMonitoring
+     */
+    private function buildEmptyItemForDay(User $operator, \DateTime $day)
+    {
         $emptyItem = new PresenceMonitoring();
         $emptyItem
-            ->setDate(new \DateTime())
             ->setWorker($operator)
-            ->setTotalHours(3)
+            ->setDate($day)
+            ->setCategory(PresenceMonitoringCategoryEnum::WORKDAY)
         ;
-        array_push($items, $emptyItem);
-        $fullYearMonthCalendarItems = array();
 
-        return $items;
+        return $emptyItem;
     }
 }
