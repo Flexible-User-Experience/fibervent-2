891ab014597cefa021e4a23a445b1a455e56e642
David Romaní <david@flux.cat>
2020-01-14 19:58:22 +0100

    add Repair admin

---
+++ src/Admin/RepairAdmin.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * Class RepairAdmin.
+ *
+ * @category Admin
+ */
+class RepairAdmin extends AbstractBaseAdmin
+{
+    protected $classnameLabel = 'admin.repair.title';
+    protected $baseRoutePattern = 'workorders/repair';
+    protected $datagridValues = array(
+        '_sort_by' => 'name',
+        '_sort_order' => 'asc',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('admin.common.general', $this->getFormMdSuccessBoxArray(4))
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'admin.common.name',
+                    'required' => true,
+                )
+            )
+            ->end()
+            ->with('admin.common.controls', $this->getFormMdSuccessBoxArray(3))
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'admin.common.enabled',
+                    'required' => false,
+                )
+            )
+            ->end()
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'admin.common.name',
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'admin.common.enabled',
+                )
+            )
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add(
+                'name',
+                null,
+                array(
+                    'label' => 'admin.common.namel',
+                    'editable' => true,
+                )
+            )
+            ->add(
+                'enabled',
+                null,
+                array(
+                    'label' => 'admin.common.enabled',
+                    'editable' => true,
+                    'header_class' => 'text-center',
+                    'row_align' => 'center',
+                )
+            )
+            ->add(
+                '_action',
+                'actions',
+                array(
+                    'label' => 'admin.common.action',
+                    'header_class' => 'text-right',
+                    'row_align' => 'right',
+                    'actions' => array(
+                        'edit' => array('template' => 'Admin/Buttons/list__action_edit_button.html.twig'),
+                        'delete' => array('template' => 'Admin/Buttons/list__action_delete_button.html.twig'),
+                    ),
+                )
+            )
+        ;
+    }
+}
