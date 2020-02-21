0b1c6b6af22ebafc3d59a01f77ca64e23403d325
David Romaní <david@flux.cat>
2020-02-21 13:32:31 +0100

    improve delivery note related actions - 7

--- src/Admin/DeliveryNoteAdmin.php
+++ src/Admin/DeliveryNoteAdmin.php
@@ -52,6 +52,7 @@ class DeliveryNoteAdmin extends AbstractBaseAdmin
             ->remove('batch')
             ->add('getWindfarmsFromWorkOrderId', $this->getRouterIdParameter().'/get-windfarms-from-work-order-id')
             ->add('pdf', $this->getRouterIdParameter().'/pdf')
+            ->add('email', $this->getRouterIdParameter().'/email')
         ;
     }
 
@@ -691,6 +692,7 @@ class DeliveryNoteAdmin extends AbstractBaseAdmin
                     'actions' => array(
                         'edit' => array('template' => 'Admin/Buttons/list__action_edit_button.html.twig'),
                         'show' => array('template' => 'Admin/Buttons/list__action_show_button.html.twig'),
+                        'email' => array('template' => 'Admin/Buttons/list__action_email_button.html.twig'),
                         'pdf' => array('template' => 'Admin/Buttons/list__action_pdf_windfarm_button.html.twig'),
                         'create_timesheet' => array('template' => 'Admin/Buttons/list__action_create_new_worker_timesheet_from_delivery_note_button.html.twig'),
                         'delete' => array('template' => 'Admin/Buttons/list__action_super_admin_delete_button.html.twig'),
